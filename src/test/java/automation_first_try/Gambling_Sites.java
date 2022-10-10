a092da2f8c41aaa441092b1e533a443aefaf407d
vmarapak <vmarapak@gmail.com>
2022-10-09 20:30:35 -0400

    my first automation code

---
+++ src/test/java/automation_first_try/Gambling_Sites.java
@@ -0,0 +1,10 @@
+package automation_first_try;
+
+public class Gambling_Sites {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- src/test/java/automation_first_try/Sports_Websites.java
+++ src/test/java/automation_first_try/Sports_Websites.java
@@ -1,37 +1,37 @@
 package automation_first_try;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-public class My_First_Automation_Code {
+
+public class Sports_Websites {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		WebDriverManager.chromedriver().setup();
 		WebDriver driver = new ChromeDriver();
-		driver.get("https://BMW.com");
+		driver.get("https://espn.com");
 		driver.manage().window().maximize();
 		
 		WebDriverManager.chromedriver().setup();
-		WebDriver driver2 = new ChromeDriver();
-		driver2.get("https://twitter.com");
-		driver2.manage().window().maximize();
-	
-		WebDriverManager.chromedriver().setup();
 		WebDriver driver3 = new ChromeDriver();
-		driver3.get("https://gmail.com");
+		driver3.get("https://nfl.com");
 		driver3.manage().window().maximize();
-		
-		
-		
-		
-		
 		
 		WebDriverManager.firefoxdriver().setup();
 		WebDriver driver1 = new FirefoxDriver();
+		driver1.get("https://nba.com");
 		driver1.manage().window().maximize();
-		driver1.get("https://youtube.com");
+		
+		WebDriverManager.firefoxdriver().setup();
+		WebDriver driver2 = new FirefoxDriver();
+		driver2.get("https://yahoosports.com");
+		driver2.manage().window().maximize();
+		
+		
+		
 		
 	}
 
